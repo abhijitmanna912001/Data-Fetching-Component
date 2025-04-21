103ced1faf03a15d514e9b5911b2a31840e10df3
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-21 16:00:29 +0530

    DataFetcher component completed

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,4 +1,4 @@
-import type { ReactElement } from "react";
+import { JSX, useEffect, useState } from "react";
 
 interface DataFetcherProps<T> {
   url: string;
@@ -6,11 +6,30 @@ interface DataFetcherProps<T> {
     data: T | null,
     isLoading: boolean,
     error: string | null
-  ) => ReactElement;
+  ) => JSX.Element;
 }
 
 function DataFetcher<T>({ url, render }: DataFetcherProps<T>) {
-  return <div>DataFetcher</div>;
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return render(data, isLoading, error);
 }
 
 export default DataFetcher;
